5411b0f39d7f815dfd1d883c7c0ec3fa97090d9c
ITRL-018\YuRuosong <ruosong.yu@sh-kelai.com>
2017-10-31 16:52:45 +0800

    no message

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
 class BlogPost(models.Model):
-    title = models.CharField(u'标题',max_length=150)
+    title = models.CharField(u'标题', max_length=150)
     body = models.TextField()
     timestamp = models.DateTimeField(u'创建时间')
+    class Meta:
+        ordering = ('-timestamp',)
